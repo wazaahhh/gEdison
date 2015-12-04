7c13a2a94973aa8c8520e2ba78f527ad482c8bb7
Thomas Maillart <thomas.maillart@gmail.com>
2015-12-03 21:11:08 -0800

    updated multiple gyroscope handling
    with connection / deconnection / reconnection

--- multipleIMUs.js
+++ multipleIMUs.js
@@ -4,47 +4,45 @@
 var noble = require('noble');
 var fs = require('fs');
 
-var imuDic =  {"9f3f64afce1545b5b1ce67a082075aeb" : "Bohr",
-               "40028eb548474d8d9b0b5c0d9c3a8350" : "Einstein"}
+var imuDic =  {'9f3f64afce1545b5b1ce67a082075aeb' : 'Bohr',
+               'e0bee4423f2c' : 'Bohr',
+               '40028eb548474d8d9b0b5c0d9c3a8350' : 'Einstein',
+                'de6d99f99c47' : 'Einstein'
+                }
 
 
 
-filename = "test_" + Date.now() + ".csv";
-filename = "test.csv";
+
+filename = "ypr_" + Date.now() + ".csv";
+filename = "ypr.csv";
 fs.appendFile(filename, "timestamp, IMU, yaw, pitch, roll,rssi\n", function (err){if (err) throw err;});
 
-// var SerialPort = require('serialport').SerialPort;
-// var serial = new SerialPort("/dev/tty.usbserial", {
-//   baudrate: 4800
-//   //parser: SerialPort.parsers.readline("\n")
-// });
-
+logfile = 'logfile';
 
 
 var state = 'poweredOn';
 
 
-NORDIC_NRF8001_SERVICE_UART = '6e400001b5a3f393e0a9e50e24dcca9e'; // Custom UART service
-NORDIC_NRF8001_CHAR_TX      = '6e400002b5a3f393e0a9e50e24dcca9e'; // Write
-NORDIC_NRF8001_CHAR_RX      = '6e400003b5a3f393e0a9e50e24dcca9e'; // Read
+var NORDIC_NRF8001_SERVICE_UART = '6e400001b5a3f393e0a9e50e24dcca9e'; // Custom UART service
+var NORDIC_NRF8001_CHAR_TX      = '6e400002b5a3f393e0a9e50e24dcca9e'; // Write
+var NORDIC_NRF8001_CHAR_RX      = '6e400003b5a3f393e0a9e50e24dcca9e'; // Read
 
 
 
 var streamData = function(peripheral) {
   peripheral.discoverSomeServicesAndCharacteristics([],[NORDIC_NRF8001_CHAR_TX,NORDIC_NRF8001_CHAR_RX],function(error, services, characteristics){
-    //services.forEach(function(service) {
-    //    console.log('found service:', service.uuid);
-    //    })
 
     characteristics.forEach(function(characteristic) {
         console.log('found characteristic:', characteristic.uuid);
 
         if (characteristic.uuid == NORDIC_NRF8001_CHAR_RX) {
+            //console.log(characteristic);
             characteristic.notify(true,function(err){
-                console.log('characteristics RX notify');
+              fs.appendFile(logfile, Date.now() + "," +  "characteristics RX notify : reading" +
+imuDic[peripheral.uuid] + "\n");
+              console.log('characteristics RX notify : reading ' +
+imuDic[peripheral.uuid]);
 
-
-                //var fh = fs.open("ypr_export.csv", 'a'); // Open the file for writing
 
                 characteristic.on('read', function(data){
 
@@ -67,7 +65,7 @@ var streamData = function(peripheral) {
                       + ", roll: " + yprRecord['roll']
                       + ", RSSI:" + yprRecord['rssi']
                       + ", IMU:" + imuDic[peripheral.uuid];
-                    console.log(str);
+
                     fs.appendFile(filename, yprRecord['timestamp']
                                     + "," + imuDic[peripheral.uuid]
                                     + "," + yprRecord['yaw']
@@ -75,7 +73,7 @@ var streamData = function(peripheral) {
                                     + "," + yprRecord['roll']
                                     + "," + yprRecord['rssi'] + "\n", function (err){
                       if (err) throw err;
-                    });// Write the string to a file
+                    });
                 });
             });
         }
@@ -83,22 +81,7 @@ var streamData = function(peripheral) {
   });
 }
 
-var connect = function(peripheral) {
-  peripheral.connect(function(err) {
-    console.log(imuDic[peripheral.uuid] + ": " + peripheral.state);
-    streamData(peripheral);
-  });
 
-  peripheral.on('disconnect', function(err) {
-    console.log(imuDic[peripheral.uuid] + ": " + peripheral.state);
-    //if (peripheral.state == "connecting") {
-    //  peripheral.disconnect();
-    //  console.log("blah");
-    //}
-
-    connect(peripheral);
-  });
-}
 
 noble.on('stateChange', function(state) {
   if (state == 'poweredOn') {
@@ -107,6 +90,8 @@ noble.on('stateChange', function(state) {
     // to begin scanning for services. Pass an empty array to
     // scan for all services (uses more time and power).
     //
+
+    fs.appendFile(logfile, Date.now() + " start scanning\n")
     console.log('scanning...');
     noble.startScanning([NORDIC_NRF8001_SERVICE_UART], false);
 
@@ -114,6 +99,8 @@ noble.on('stateChange', function(state) {
   }
   else {
     noble.stopScanning();
+    fs.appendFile(logfile, Date.now() +  "stop scanning\n")
+
   }
 })
 
@@ -121,6 +108,24 @@ noble.on('stateChange', function(state) {
 
 
 noble.on('discover', function(peripheral) {
-  connect(peripheral);
-  streamData(peripheral);
+
+  peripheral.connect(function(err) {
+    fs.appendFile(logfile, Date.now() + "," + imuDic[peripheral.uuid] + "," + peripheral.state + "\n")
+    console.log(imuDic[peripheral.uuid] + ": " + peripheral.state);
+    streamData(peripheral);
+
+    if (err) {
+      console.log("error");
+    }
+  });
+
+  peripheral.on('disconnect', function(err) {
+    fs.appendFile(logfile, Date.now() + "," + imuDic[peripheral.uuid] + "," + peripheral.state + "\n")
+     console.log(imuDic[peripheral.uuid] + ": " + peripheral.state);
+     peripheral.connect(function(err) {
+       fs.appendFile(logfile, Date.now() + "," + imuDic[peripheral.uuid] + "," + peripheral.state + "\n")
+       console.log(imuDic[peripheral.uuid] + ": " + peripheral.state);
+       streamData(peripheral);
+    });
+  });
 });
